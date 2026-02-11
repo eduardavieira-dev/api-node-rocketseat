e7a97a90ac3d14debd5b8aea82961c119956aa5a
Eduarda Vieira <eduarda.vieira.goncalves7@gmail.com>
2026-02-10 21:00:26 -0300

    chore: update code coverage timestamps and remove unused test setup files

--- src/tests/setup.ts
+++
@@ -1,8 +0,0 @@
-// Importa todos os arquivos para garantir cobertura
-import '../routes/create-course.ts'
-import '../routes/get-courses.ts'
-import '../routes/get-course-by-id.ts'
-import '../routes/update-course.ts'
-import '../routes/delete-course.ts'
-import '../database/client.ts'
-import '../database/schema.ts'
